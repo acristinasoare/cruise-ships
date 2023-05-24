851da9d7ae0c2208c69c6e2ea75e8a7d76df708b
Cristina Soare <kiti.soare@ymail.com>
2023-05-24 20:43:35 +0100

    added setSail method and passed corresponding test

--- cruise-ships/src/cruise-ships.js
+++ cruise-ships/src/cruise-ships.js
@@ -2,6 +2,10 @@ class Ship {
     constructor(startingPort) { 
         this.startingPort = startingPort;
     }
+
+    setSail() {
+        this.startingPort = null;
+    }
 }
 
 module.exports = Ship;
